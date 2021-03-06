61e07b6d5399736d465b2d883e10cf3b83201e8d
papryka1 <kasiukas2@gmail.com>
2021-03-06 19:21:53 +0200

    File Upload Database

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,26 +1,13 @@
-const rootStyles = window.getComputedStyle(document.documentElement)
+FilePond.registerPlugin(
+  FilePondPluginImagePreview,
+  FilePondPluginImageResize,
+  FilePondPluginFileEncode,
+)
 
-if (rootStyles.getPropertyValue('--book-cover-width-large') != null && rootStyles.getPropertyValue('--book-cover-width-large') !== '') {
-  ready()
-} else {
-  document.getElementById('main-css').addEventListener('load', ready)
-}
+FilePond.setOptions({
+  stylePanelAspectRatio: 150 / 100,
+  imageResizeTargetWidth: 100,
+  imageResizeTargetHeight: 150
+})
 
-function ready() {
-  const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'))
-  const coverAspectRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio'))
-  const coverHeight = coverWidth / coverAspectRatio
-  FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode,
-  )
-
-  FilePond.setOptions({
-    stylePanelAspectRatio: 1 / coverAspectRatio,
-    imageResizeTargetWidth: coverWidth,
-    imageResizeTargetHeight: coverHeight
-  })
-  
-  FilePond.parse(document.body)
-}
+FilePond.parse(document.body);
